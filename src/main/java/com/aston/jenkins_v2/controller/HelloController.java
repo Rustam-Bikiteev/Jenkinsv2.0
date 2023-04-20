8e5246d4b7c92602bc1e561e284768451ec1d304
Rustam Bikiteev <rust.bikiteev@gmail.com>
2023-04-20 11:47:48 +0200

    Change output v2

--- src/main/java/com/aston/jenkins_v2/controller/HelloController.java
+++ src/main/java/com/aston/jenkins_v2/controller/HelloController.java
@@ -1,2 +1,12 @@
-package com.aston.jenkins_v2.controller;public class HelloController {
+package com.aston.jenkins_v2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello world2";
+    }
 }
