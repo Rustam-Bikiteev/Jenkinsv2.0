8f387d5459ec85f226bae83247e0098ac842e0c7
Rustam Bikiteev <rust.bikiteev@gmail.com>
2023-04-21 10:10:26 +0200

    commit Final

--- src/main/java/com/aston/jenkins_v2/controller/HelloController.java
+++ src/main/java/com/aston/jenkins_v2/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello(){
-        return "new comment 6";
+        return "FINAL RESULT!!!!";
     }
 }
